fb2becc89415e03abae471f29fa9ad2cf474ed7d
unknown <Romangergisevich@gmail.com>
2024-03-13 14:13:50 +0300

    false currency highlight task

--- src/App.vue
+++ src/App.vue
@@ -20,8 +20,8 @@
         </div>
         <button type="button" @click="add()"
           class="my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
-          <svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24"
-            fill="#ffffff">
+          <svg class="-ml-0.5 mr-2 h-6 w-6" xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+            viewBox="0 0 24 24" fill="#ffffff">
             <path
               d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z">
             </path>
@@ -47,7 +47,7 @@
           <div v-for="t in paginatedTickers" :key="t.name" @click="select(t)"
             :class="selectedTicker == t ? 'border-4' : ''"
             class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer">
-            <div class="px-4 py-5 sm:p-6 text-center">
+            <div :class="formatPrice(t.price) == '-' ? 'bg-red-300' : 'bg-white'" class="px-4 py-5 sm:p-6 text-center">
               <dt class="text-sm font-medium text-gray-500 truncate">
                 {{ t.name }} - USD
               </dt>
